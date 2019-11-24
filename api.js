dd2a37e6ce32b62ebf2e1e44e95bdb6fd739e5b6
Godspower Omenihu <ask4godsomex@gmail.com>
2019-11-24 22:37:09 +0100

    refactor code

--- api.js
+++ api.js
@@ -1,29 +1,55 @@
-let dropdown = document.getElementById("locality-dropdown");
-dropdown.length = 0;
+function myFunction() {
+  const dropdown = document.getElementById("locality-dropdown");
+  dropdown.length = 0;
 
-let defaultOption = document.createElement("option");
-defaultOption.text = "Choose State/Province";
+  const postcode = document.getElementById("plz").value;
 
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
+  const stadt = document.getElementById("stadt");
 
-let url =
-  "https://cors-anywhere.herokuapp.com/https://www.postdirekt.de/plzserver/PlzAjaxServlet";
+  const defaultOption = document.createElement("option");
+  defaultOption.text = "Choose State/Province";
 
-let postcode = "81241";
-let params = "finda=city&city=" + postcode + "&lang=de_DE";
+  dropdown.add(defaultOption);
+  dropdown.selectedIndex = 0;
 
-try {
-  fetch(`${url}?${params}`)
-    .then(res => res.json())
-    .then(result => {
-      console.log(result);
-      let option;
-      option = document.createElement("option");
-      option.text = result.rows[0].city;
-      option.value = result.rows[0].city;
-      dropdown.add(option);
-    });
-} catch (error) {
-  console(error);
+  const url =
+    "https://cors-anywhere.herokuapp.com/https://www.postdirekt.de/plzserver/PlzAjaxServlet";
+  const params = "finda=city&city=" + postcode + "&lang=de_DE";
+
+  function getCity(a, b) {
+    return fetch(`${a}?${b}`);
+  }
+
+  if (postcode.length == 5) {
+    getCity(url, params)
+      .then(res => res.json())
+      .then(result => {
+        let option;
+        option = document.createElement("option");
+        option.text = result.rows[0].city;
+        option.value = result.rows[0].city;
+        dropdown.add(option);
+        stadt.value = result.rows[0].city;
+      })
+      .catch(err => console.log(err));
+  } else {
+    stadt.value = "please enter 5 digits";
+  }
+
+  const formToJSON = elements =>
+    elements.reduce((data, element) => {
+      data[element.name] = element.value;
+      return data;
+    }, {});
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    const data = formToJSON(Array.from(form.elements));
+    const dataContainer = document.getElementsByTagName("section")[0];
+    dataContainer.textContent = JSON.stringify(data, null, "  ");
+  };
+
+  const form = document.getElementsByTagName("form")[0];
+
+  form.addEventListener("submit", handleFormSubmit);
 }
